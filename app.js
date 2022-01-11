eadb7bfc38d2fd20cc5fd8e86b37b6ea9eefa671
nicocran1911 <93523661+nicocran1911@users.noreply.github.com>
2022-01-11 11:52:32 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,65 @@
+const menu = {
+ _courses: {
+   appetizers: [],
+   mains: [],
+   desserts: [],
+ },
+ get appetizers() {
+   return this._courses.appetizers;
+ },
+ get mains() {
+   return this._courses.mains;
+ },
+ get desserts() {
+   return this._courses.desserts;
+ },
+ set appetizers(appetizers) {
+   this._courses.appetizers = appetizers;
+ },
+ set mains(mains) {
+   this._courses.mains = mains;
+ },
+ set desserts(desserts) {
+   this._courses.desserts = desserts;
+ },
+ get courses() {
+   return {
+     appetizers: this.appetizers,
+     mains: this.mains,
+     desserts: this.desserts,
+   };
+ },
+  addDishToCourse (courseName, dishName, dishPrice) {
+    const dish = {
+      name: dishName,
+      price: dishPrice,
+    };
+    return this._courses[courseName].push(dish);
+  },
+  getRandomDishFromCourse(courseName) {
+    const dishes = this._courses[courseName];
+    const randomIndex = Math.floor(Math.random() *  dishes.length);
+    return dishes [randomIndex];
+  },
+  generateRandomMeal(){
+    const appetizer = this.getRandomDishFromCourse('appetizers');
+    const main = this.getRandomDishFromCourse('mains');
+    const dessert = this.getRandomDishFromCourse('desserts');
+    const totalPrice = appetizer.price + main.price + dessert.price;
+    return `Your meal is ${appetizer.name}, ${main.name}, and ${dessert.main}, and the total price is ${totalPrice}`;
+  }
+};
+menu.addDishToCourse('appetizers', 'soup', 2.00);
+menu.addDishToCourse('appetizers', 'fries', 4.00);
+menu.addDishToCourse('appetizers', 'fish', 2.00);
+
+menu.addDishToCourse('mains', 'rice', 7.00);
+menu.addDishToCourse('mains', 'ice cream', 2.50);
+menu.addDishToCourse('mains', 'banku', 5.00);
+
+menu.addDishToCourse('desserts', 'salad', 12.00);
+menu.addDishToCourse('desserts', 'salad', 2.10);
+menu.addDishToCourse('desserts', 'fruit', 8.00);
+
+const meal = menu.generateRandomMeal();
+console.log(meal);
